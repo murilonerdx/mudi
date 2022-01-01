2dfe44d9b40c8c2c881f4508ced33588b9a051fe
murilonerdx <mu-silva@outlook.com>
2022-01-01 15:48:54 -0300

    Adicionando JPA puro e injeção de dados

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -5,22 +5,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class HomeController {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @GetMapping("/home")
     public String home(Model model) {
-        Pedido pedido = new Pedido();
-        pedido.setNomeProduto("Xiaomi Redmi Note 8");
-        pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/81UgYuadkpL._AC_SL1500_.jpg");
-        pedido.setUrlProduto("https://www.amazon.com.br/Smartphone-Xiaomi-Redmi-Note-64GB/dp/B07Y8YWTFL/ref=sr_1_6?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=Xiaomi+Redmi+Note+8&qid=1600346040&sr=8-6");
-        pedido.setDescricao("uma descrição qualquer para esse pedido");
+//        Pedido pedido = new Pedido();
+//        pedido.setNomeProduto("Xiaomi Redmi Note 8");
+//        pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/81UgYuadkpL._AC_SL1500_.jpg");
+//        pedido.setUrlProduto("https://www.amazon.com.br/Smartphone-Xiaomi-Redmi-Note-64GB/dp/B07Y8YWTFL/ref=sr_1_6?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=Xiaomi+Redmi+Note+8&qid=1600346040&sr=8-6");
+//        pedido.setDescricao("uma descrição qualquer para esse pedido");
 
-        List<Pedido> pedidos = Arrays.asList(pedido);
-        model.addAttribute("pedidos", pedidos);
+        Query select_p_from_pedido_p = entityManager.createQuery("select p from Pedido p", Pedido.class);
+        List<Pedido> resultList = select_p_from_pedido_p.getResultList();
+
+//        List<Pedido> pedidos = Arrays.asList(pedido);
+        model.addAttribute("pedidos", resultList);
 
         return "home";
     }
--- src/main/java/br/com/alura/mvc/mudi/entities/Pedido.java
+++ src/main/java/br/com/alura/mvc/mudi/entities/Pedido.java
@@ -13,23 +13,28 @@ public class Pedido {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
     private String nomeProduto;
-    private BigDecimal valorNegociacao;
+    private BigDecimal valorNegociado;
     private LocalDate dataDaEntrega;
     private String urlProduto;
     private String urlImagem;
     private String descricao;
 
-    public Pedido(String nomeProduto, BigDecimal valorNegociacao, LocalDate dataDaEntrega, String urlProduto, String urlImagem, String descricao) {
+    public Pedido(Long id, String nomeProduto, BigDecimal valorNegociado, LocalDate dataDaEntrega, String urlProduto, String urlImagem, String descricao) {
+        this.id = id;
         this.nomeProduto = nomeProduto;
-        this.valorNegociacao = valorNegociacao;
+        this.valorNegociado = valorNegociado;
         this.dataDaEntrega = dataDaEntrega;
         this.urlProduto = urlProduto;
         this.urlImagem = urlImagem;
         this.descricao = descricao;
     }
 
-    public Pedido() {
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getNomeProduto() {
@@ -40,12 +45,12 @@ public class Pedido {
         this.nomeProduto = nomeProduto;
     }
 
-    public BigDecimal getValorNegociacao() {
-        return valorNegociacao;
+    public BigDecimal getValorNegociado() {
+        return valorNegociado;
     }
 
-    public void setValorNegociacao(BigDecimal valorNegociacao) {
-        this.valorNegociacao = valorNegociacao;
+    public void setValorNegociado(BigDecimal valorNegociado) {
+        this.valorNegociado = valorNegociado;
     }
 
     public LocalDate getDataDaEntrega() {
@@ -80,11 +85,8 @@ public class Pedido {
         this.descricao = descricao;
     }
 
-    public Long getId() {
-        return id;
+    public Pedido() {
+
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
