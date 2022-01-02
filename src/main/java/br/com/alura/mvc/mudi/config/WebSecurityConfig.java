b3623144d1af1a3ed0ca8552496f95ad7052b5ae
murilonerdx <mu-silva@outlook.com>
2022-01-02 20:18:48 -0300

    Criando pagina de login, criando mapeamento e configuração no spring security

--- src/main/java/br/com/alura/mvc/mudi/config/WebSecurityConfig.java
+++ src/main/java/br/com/alura/mvc/mudi/config/WebSecurityConfig.java
@@ -17,13 +17,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/home")
+                        .permitAll()
+                )
                 .logout()
                 .permitAll();
     }
